653f6de3754710e609cdc189cb492bf6b22f4055
jota <jvfranco08@gmail.com>
2025-06-15 22:27:07 -0300

    loucura

--- src/pages/Orders.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import AppointmentsDashboard from '@/components/appointments/AppointmentsDashboard';
-
-const Orders = () => {
-  return <AppointmentsDashboard />;
-};
-
-export default Orders;
